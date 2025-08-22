42fa09e845fc7fccfc3ba03d7fec3114ccf3e8cb
AgustinFrich <agustinfrich@gmail.com>
2025-08-21 21:56:54 -0300

    fin ruteo

--- clase-02/src/app/pages/usuarios/usuarios.ts
+++ clase-02/src/app/pages/usuarios/usuarios.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+// import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-usuarios',
-  imports: [],
+  // imports: [NgIf],
   templateUrl: './usuarios.html',
   styleUrl: './usuarios.css',
 })
 export class Usuarios {
+  usuarios = [
+    {
+      nombre: 'Agus',
+      perfil: 'Profesor',
+    },
+    {
+      nombre: 'Pepito',
+      perfil: 'Estudiante',
+    },
+    {
+      nombre: 'Lo que sea',
+      perfil: 'Estudiante',
+    },
+    {
+      nombre: 'Otro',
+      perfil: 'Estudiante',
+    },
+  ];
+  usuarioActual: number = 0;
+
+  private activatedRoute = inject(ActivatedRoute);
+
+  // /usuarios/123
+  // /usuarios/1234
+  // ActivatedRoute
+
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
@@ -17,5 +45,31 @@ export class Usuarios {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
     console.log('Se destruye el componente usuarios');
+  }
+
+  ngOnChanges() {
+    console.log('Changes');
+  }
+
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    console.log('Check');
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.usuarioActual = parseInt(id);
+    }
+  }
+
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log('Init content');
+  }
+  ngAfterContentChecked(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log('Checked content');
   }
 }
