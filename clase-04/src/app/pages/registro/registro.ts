504c6d6c182b1030b964538c45bae8e1ebf60a35
AgustinFrich <agustinfrich@gmail.com>
2025-08-28 19:45:31 -0300

    Formulario sin lógica

--- clase-04/src/app/pages/registro/registro.ts
+++ clase-04/src/app/pages/registro/registro.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro.html',
-  styleUrl: './registro.css'
+  styleUrl: './registro.css',
 })
 export class Registro {
+  nombre = new FormControl('');
+  // apellido = new FormControl('');
+  // correo = new FormControl('');
+  // contrasenia = new FormControl('');
+  // repetirContrasenia = new FormControl('');
 
+  formulario = new FormGroup({
+    nombre: this.nombre,
+    apellido: new FormControl(''),
+    correo: new FormControl(''),
+    contrasenia: new FormControl(''),
+    repetirContrasenia: new FormControl(''),
+  });
+
+  // login = new FormGroup({
+  //   correo: new FormControl(''),
+  // contrasenia: new FormControl(''),
+  // });
 }
