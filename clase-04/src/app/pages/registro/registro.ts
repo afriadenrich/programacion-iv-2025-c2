dd26d9eb2b6ed5e91839a121a9ff5fbc34f2c0ed
AgustinFrich <agustinfrich@gmail.com>
2025-08-28 20:29:12 -0300

    recreo

--- clase-04/src/app/pages/registro/registro.ts
+++ clase-04/src/app/pages/registro/registro.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,7 +15,11 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './registro.css',
 })
 export class Registro {
-  nombre = new FormControl('');
+  nombre = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(20),
+  ]);
   // apellido = new FormControl('');
   // correo = new FormControl('');
   // contrasenia = new FormControl('');
@@ -16,14 +27,52 @@ export class Registro {
 
   formulario = new FormGroup({
     nombre: this.nombre,
-    apellido: new FormControl(''),
-    correo: new FormControl(''),
-    contrasenia: new FormControl(''),
-    repetirContrasenia: new FormControl(''),
+    apellido: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    contrasenia: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    repetirContrasenia: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      this.validarContrasenias,
+    ]),
   });
 
-  // login = new FormGroup({
-  //   correo: new FormControl(''),
-  // contrasenia: new FormControl(''),
-  // });
+  login = new FormGroup({
+    correo: new FormControl(''),
+    contrasenia: new FormControl(''),
+  });
+
+  enviarFormulario() {
+    console.log(this.formulario);
+    console.log(this.formulario.value);
+    console.log(this.formulario.get('nombre'));
+  }
+
+  validarContrasenias(control: AbstractControl): ValidationErrors | null {
+    const error = { iguales: false };
+
+    if (!control.value) {
+      return error;
+    }
+
+    if (!this.formulario) {
+      return error;
+    }
+
+    const contrasenia = this.formulario.get('contrasenia')?.value;
+
+    if (!contrasenia) {
+      return error;
+    }
+
+    if (control.value === contrasenia) {
+      return null;
+    } else {
+      return error;
+    }
+  }
 }
