bdafa339af3bd76bff2bc9d0b9d13df8e2ed2a90
AgustinFrich <agustinfrich@gmail.com>
2025-10-08 21:48:31 -0300

    saco user module

--- clase10-nest/src/app.module.ts
+++ clase10-nest/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RopaController } from './controllers/ropa/ropa.controller';
-import { UsuarioModule } from './modules/usuario/usuario.module';
 import { LibroModule } from './resources/libro/libro.module';
 
 @Module({
-  imports: [UsuarioModule, LibroModule],
+  imports: [LibroModule],
   controllers: [AppController, RopaController],
   providers: [AppService],
   exports: [],
