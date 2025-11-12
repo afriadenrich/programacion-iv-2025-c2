a19385103eab8f3d9623a083e887e910fdaad448
AgustinFrich <agustinfrich@gmail.com>
2025-11-12 20:41:10 -0300

    Webscokets

--- Clase17-Sockets/front/src/app/app.ts
+++ Clase17-Sockets/front/src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { io, Socket } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [FormsModule],
 })
 export class App {
-  protected readonly title = signal('front');
+  mensajes = signal<string[]>([]);
+
+  mensaje = '';
+
+  client: Socket;
+
+  constructor() {
+    this.client = io('ws://localhost:3000/');
+
+    this.client.on('mensajeRecibido', (data) => {
+      this.mensajes.update((anterior) => {
+        return [...anterior, data];
+      });
+    });
+  }
+
+  enviar() {
+    this.client.emit('enviarMensaje', this.mensaje);
+    console.log('Envia el mensaje');
+  }
 }
